8600f2989b9bb4c6ba4bba1537cf983634dce08e
Mohammed Muzammil Anwar <mohammad.muzammil100@gmail.com>
2023-04-05 21:28:44 +0530

    updated uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -1,14 +1,16 @@
 # uninstall script for dotfiles by @mcking-07
 
-# DOT_CONFIG_FOLDERS='starship, nvim'
-# DOT_FOLDERS='bash, git'
-DOT_FOLDERS='git'
+# define the list of dotfile and config folders
+DOT_CONFIG_FOLDERS='starship, nvim'
+DOT_FOLDERS='bash, git'
 
+# loop through dotconfig folders and remove symlinks
 for folder in $(echo $DOT_CONFIG_FOLDERS | sed "s/,/ /g"); do
   echo "[+] $folder"
   stow -v -t $HOME/.config/$folder $folder &>/dev/null
 done
 
+# loop through dotfile folders and remove symlinks
 for folder in $(echo $DOT_FOLDERS | sed "s/,/ /g"); do
   echo "[+] $folder"
   stow -t $HOME -D $folder &>/dev/null
